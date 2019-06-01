8a67b01b57974ea439630b60cd709333792ec4ba
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-06-01 21:15:53 +0900

    エラーなく学習が完了できる

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
+import cv2
+import numpy as np
 import os
 import re
 import matplotlib.pyplot as plt
 from keras.utils.np_utils import to_categorical
 from keras.layers import Activation, Conv2D, Dense, Flatten, MaxPooling2D
 from keras.models import Sequential
+from keras.preprocessing.image import img_to_array, load_img
 
 members = [
   "akariuemura",
@@ -18,41 +21,39 @@ member_base_dir = "members"
 test_base_dir = "test"
 
 # 訓練
-x_train = []
+X_train = []
 y_train = []
 for i, member in enumerate(members):
     member_dir = member_base_dir + '/' + member + '/'
     for filename in os.listdir(member_dir):
         if not re.match('.+.jpg', filename): continue
         filepath = member_dir + filename
-        img = cv2.imread(filepath)
-        r, g, b = cv2.split(img)
-        img = cv2.merge([r, g, b])
-        x_train.append(img)
+        img = img_to_array(load_img(filepath, target_size=(64,64)))
+        X_train.append(img)
         y_train.append(i)
 
 # テストデータ
-x_test = []
+X_test = []
 y_test = []
 for i, member in enumerate(members):
     member_dir = test_base_dir + '/' + member + '/'
     for filename in os.listdir(member_dir):
         if not re.match('.+.jpg', filename): continue
         filepath = member_dir + filename
-        img = cv2.imread(filepath)
-        r, g, b = cv2.split(img)
-        img = cv2.merge([r, g, b])
-        x_test.append(img)
+        img = img_to_array(load_img(filepath, target_size=(64,64)))
+        X_test.append(img)
         y_test.append(i)
 
-x_train = np.array(x_train)
-x_test  = np.array(x_test)
+X_train = np.asarray(X_train)
+X_test  = np.asarray(X_test)
 y_train = to_categorical(y_train)
 y_test  = to_categorical(y_test)
 
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
 # モデルの定義
 model = Sequential()
-model.add(Conv2D(input_shape=(200, 200, 3), filters=32, kernel_size=(3, 3), strides=(1, 1), padding="same"))
+model.add(Conv2D(input_shape=X_train.shape[1:], filters=32, kernel_size=(3, 3), strides=(1, 1), padding="same"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), padding="same"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -70,13 +71,15 @@ model.add(Activation('softmax'))
 model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # 学習
-history = model.fit(x_train, y_train, batch_size=70, epochs=5, verbose=1, validation_data=(x_test, y_test))
+history = model.fit(X_train, y_train, batch_size=70, epochs=5, verbose=1, validation_data=(X_test, y_test))
 
 #モデルを保存
-model.save("YukanyaModel.h5")
+model_json = model.to_json()
+open('YukanyaModel.json', 'w').write(model_json)
+model.save_weights("YukanyaModel.h5")
 
 # 汎化制度の評価・表示
-score = model.evaluate(x_test, y_test, batch_size=32, verbose=0)
+score = model.evaluate(X_test, y_test, batch_size=32, verbose=0)
 print('validation loss:{0[0]}\nvalidation accuracy:{0[1]}'.format(score))
 
 #acc, val_accのプロット
