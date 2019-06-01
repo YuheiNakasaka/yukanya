74bff9a29121f3d3c019f6f88fccc2a9967dfdff
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-06-01 13:16:55 +0900

    画像をガツっと取ってくる

--- scraping.py
+++ scraping.py
@@ -7,30 +7,45 @@ import shutil
 import requests
 import hashlib
 import time
+import sys
 
 options = Options()
 options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 
-driver.get('https://ameblo.jp/juicejuice-official/imagelist-201905.html')
+year = 2019
+month = 5
+i = 24 # 2年分
+while i > 0:
+    url = 'https://ameblo.jp/juicejuice-official/imagelist-' + str(year) + str(month).rjust(2, "0") + '.html'
+    print('Link: ' + url)
+    driver.get(url)
+    html = driver.page_source.encode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
 
-html = driver.page_source.encode('utf-8')
-soup = BeautifulSoup(html, 'html.parser')
+    dl_image_path = 'dl_images'
 
-dl_image_path = 'dl_images'
+    if not os.path.exists(dl_image_path):
+        os.mkdir(dl_image_path)
 
-if not os.path.exists(dl_image_path):
-    os.mkdir(dl_image_path)
+    imgs = soup.find_all("img", attrs={"class": "imgItem"})
+    for j, elm in enumerate(imgs):
+        sys.stdout.write('\r')
+        sys.stdout.write('%-20s' % ('.' * j))
+        sys.stdout.flush()
+        src = elm['src']
+        filename = hashlib.sha256(src.encode()).hexdigest()
+        resp = requests.get(src, stream=True)
+        with open(dl_image_path + '/' + filename + '.jpg', 'ab') as fp:
+            shutil.copyfileobj(resp.raw, fp)
+        time.sleep(1)
+    sys.stdout.write('\n')
 
-imgs = soup.find_all("img", attrs={"class": "imgItem"})
-for elm in imgs:
-    print(elm['src'])
-    src = elm['src']
-    filename = hashlib.sha256(src.encode()).hexdigest()
-    resp = requests.get(src, stream=True)
-    with open(dl_image_path + '/' + filename + '.jpg', 'ab') as fp:
-        shutil.copyfileobj(resp.raw, fp)
-    time.sleep(1)
+    month -= 1
+    i -= 1
+    if month == 0:
+        year -= 1
+        month = 12
 
 driver.quit()
