92786d16f2ebbc340b59ab35c096b314fa16a94a
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2019-06-04 21:24:37 +0900

    VGG19で転移学習

--- increase_data.py
+++ increase_data.py
@@ -5,20 +5,12 @@ import re
 from scipy import ndimage
 
 def create_angle_image(img, base_membar_dir, basename, ext, suffix):
-    for angle in [-10, 0, 10]:
+    for angle in [-10, 10]:
         img = cv2.resize(img, (200, 200))
         # 回転
         rotate_img = ndimage.rotate(img, angle)
         angle_file_path = os.path.join(base_membar_dir, basename + '_' + str(angle) + '_' + suffix + ext)
         cv2.imwrite(angle_file_path, rotate_img)
-        # 閾値
-        threshold_img = cv2.threshold(rotate_img, 100, 255, cv2.THRESH_TOZERO)[1]
-        threshold_file_path = os.path.join(base_membar_dir, basename + '_' + str(angle) + '_' + suffix + '_' + 'threshold' + ext)
-        cv2.imwrite(threshold_file_path, threshold_img)
-        # Gaussian
-        gaussian_img = cv2.GaussianBlur(rotate_img, (5, 5), 0)
-        gaussian_file_path = os.path.join(base_membar_dir, basename + '_' + str(angle) + '_' + suffix + '_' + 'gaussian' + ext)
-        cv2.imwrite(gaussian_file_path, gaussian_img)
 
 members = [
   "akariuemura",
@@ -32,43 +24,51 @@ members = [
 member_image_dir = "members"
 
 for member in members:
-  print(member)
-  base_membar_dir = member_image_dir + '/' + member
-  for filename in os.listdir(base_membar_dir):
-      if not re.match('.+_\d+_\d+\.jpg', filename): continue
-      basename, ext = os.path.splitext(filename)
-      filepath = base_membar_dir + '/' + filename
-      img = cv2.imread(filepath)
-      img = cv2.resize(img, (64, 64))
+    print(member)
+    base_membar_dir = member_image_dir + '/' + member
+    for filename in os.listdir(base_membar_dir):
+        if not re.match('.+_\d+_\d+\.jpg', filename): continue
+        basename, ext = os.path.splitext(filename)
+        filepath = base_membar_dir + '/' + filename
+        img = cv2.imread(filepath)
+        img = cv2.resize(img, (64, 64))
 
-      # コントラスト調整
-      min_table = 50
-      max_table = 205
-      diff_table = max_table - min_table
-      LUT_HC = np.arange(256, dtype = 'uint8')
-      LUT_LC = np.arange(256, dtype = 'uint8')
-      for i in range(0, min_table):
-          LUT_HC[i] = 0
-      for i in range(min_table, max_table):
-          LUT_HC[i] = 255 * (i - min_table) / diff_table
-      for i in range(max_table, 255):
-          LUT_HC[i] = 255
-      for i in range(256):
-          LUT_LC[i] = min_table + i * (diff_table) / 255
-      high_cont_img = cv2.LUT(img, LUT_HC)
-      low_cont_img = cv2.LUT(img, LUT_LC)
-      high_cont_file_path = os.path.join(base_membar_dir, basename + '_highcont' + ext)
-      cv2.imwrite(high_cont_file_path, high_cont_img)
-      low_cont_file_path = os.path.join(base_membar_dir, basename + '_lowcont' + ext)
-      cv2.imwrite(low_cont_file_path, low_cont_img)
+        # # コントラスト調整
+        # min_table = 50
+        # max_table = 205
+        # diff_table = max_table - min_table
+        # LUT_HC = np.arange(256, dtype = 'uint8')
+        # LUT_LC = np.arange(256, dtype = 'uint8')
+        # for i in range(0, min_table):
+        #     LUT_HC[i] = 0
+        # for i in range(min_table, max_table):
+        #     LUT_HC[i] = 255 * (i - min_table) / diff_table
+        # for i in range(max_table, 255):
+        #     LUT_HC[i] = 255
+        # for i in range(256):
+        #     LUT_LC[i] = min_table + i * (diff_table) / 255
+        # high_cont_img = cv2.LUT(img, LUT_HC)
+        # low_cont_img = cv2.LUT(img, LUT_LC)
+        # high_cont_file_path = os.path.join(base_membar_dir, basename + '_highcont' + ext)
+        # cv2.imwrite(high_cont_file_path, high_cont_img)
+        # low_cont_file_path = os.path.join(base_membar_dir, basename + '_lowcont' + ext)
+        # cv2.imwrite(low_cont_file_path, low_cont_img)
 
-      # 左右反転
-      hflip_img = cv2.flip(img, 1)
-      flip_file_path = os.path.join(base_membar_dir, basename + '_flip' + ext)
-      cv2.imwrite(flip_file_path, hflip_img)
+        # # 左右反転
+        # hflip_img = cv2.flip(img, 1)
+        # flip_file_path = os.path.join(base_membar_dir, basename + '_flip' + ext)
+        # cv2.imwrite(flip_file_path, hflip_img)
 
-      # 回転版
-      create_angle_image(img, base_membar_dir, basename, ext, 'normal')
-      # create_angle_image(hflip_img, base_membar_dir, basename, ext, 'hflip')
-      # create_angle_image(high_cont_img, base_membar_dir, basename, ext, 'highcont')
-      # create_angle_image(low_cont_img, base_membar_dir, basename, ext, 'lowcont')
+        # 閾値
+        threshold_img = cv2.threshold(img, 100, 255, cv2.THRESH_TOZERO)[1]
+        threshold_file_path = os.path.join(base_membar_dir, basename + '_normal_' + 'threshold' + ext)
+        cv2.imwrite(threshold_file_path, threshold_img)
+        # Gaussian
+        gaussian_img = cv2.GaussianBlur(img, (5, 5), 0)
+        gaussian_file_path = os.path.join(base_membar_dir, basename + '_normal_' + 'gaussian' + ext)
+        cv2.imwrite(gaussian_file_path, gaussian_img)
+        # 回転版
+        create_angle_image(img, base_membar_dir, basename, ext, 'normal')
+        # create_angle_image(hflip_img, base_membar_dir, basename, ext, 'hflip')
+        # create_angle_image(high_cont_img, base_membar_dir, basename, ext, 'highcont')
+        # create_angle_image(low_cont_img, base_membar_dir, basename, ext, 'lowcont')
---
+++ train_vgg.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+import os
+import re
+import matplotlib.pyplot as plt
+from keras.utils.np_utils import to_categorical
+from keras.layers import Activation, Conv2D, Dense, Dropout, Flatten, MaxPooling2D, Input, GlobalAveragePooling2D
+from keras.models import Sequential, Model
+from keras.preprocessing.image import img_to_array, load_img
+from sklearn.model_selection import train_test_split
+from keras.applications.vgg19 import VGG19
+from keras.optimizers import SGD
+
+members = [
+  "akariuemura",
+  "karinmiyamoto",
+  "manakainaba",
+  "rurudanbara",
+  "sayukitakagi",
+  "tomokokanazawa",
+  "yukamiyazaki"
+]
+member_base_dir = "members"
+n_category = len(members)
+
+# 訓練データ
+X_train = []
+y_train = []
+for i, member in enumerate(members):
+    member_dir = member_base_dir + '/' + member + '/'
+    for filename in os.listdir(member_dir):
+        if not re.match('.+.jpg', filename): continue
+        filepath = member_dir + filename
+        img = img_to_array(load_img(filepath, target_size=(64,64)))
+        X_train.append(img)
+        y_train.append(i)
+
+X_train = np.asarray(X_train).astype('float32') / 255.0
+y_train = to_categorical(y_train, n_category)
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+# モデルの定義: VGG19モデル
+input_tensor = Input(shape=(64,64,3))
+vgg19_model = VGG19(include_top=False, weights='imagenet', input_tensor=input_tensor)
+
+top_model = Sequential()
+top_model.add(Flatten(input_shape=vgg19_model.output_shape[1:]))
+top_model.add(Dense(256))
+top_model.add(Activation("sigmoid"))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(128))
+top_model.add(Activation('sigmoid'))
+top_model.add(Dense(n_category))
+top_model.add(Activation("softmax"))
+
+model = Model(input=vgg19_model.input, output=top_model(vgg19_model.output))
+
+for layer in model.layers[:17]:
+    layer.trainable = False
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+model.summary()
+
+# 学習
+history = model.fit(X_train, y_train, batch_size=100, epochs=50, verbose=1, validation_data=(X_test, y_test))
+
+#モデルを保存
+model.save("YukanyaModel_vgg_all.h5")
+model_json = model.to_json()
+open('YukanyaModel_vgg.json', 'w').write(model_json)
+model.save_weights("YukanyaModel_vgg.h5")
+
+# 汎化制度の評価・表示
+score = model.evaluate(X_test, y_test, batch_size=32, verbose=0)
+print('validation loss:{0[0]}\nvalidation accuracy:{0[1]}'.format(score))
+
+#acc, val_accのプロット
+plt.plot(history.history["acc"], label="acc", ls="-", marker="o")
+plt.plot(history.history["val_acc"], label="val_acc", ls="-", marker="x")
+plt.ylabel("accuracy")
+plt.xlabel("epoch")
+plt.legend(loc="best")
+plt.show()
